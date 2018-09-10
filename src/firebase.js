e07c6c53f811471167bc0b74d95830e5f7b78937
iambigmomma <iambigmomma@gmail.com>
2018-09-10 23:20:15 +0200

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/database'
+
+const config = {
+    apiKey: "AIzaSyDFW6SDlqeoGI881Q9zO5repnnPZDjpY1U",
+    authDomain: "team-match-33f32.firebaseapp.com",
+    databaseURL: "https://team-match-33f32.firebaseio.com",
+    projectId: "team-match-33f32",
+    storageBucket: "team-match-33f32.appspot.com",
+    messagingSenderId: "217712429666"
+  };
+  firebase.initializeApp(config);
+
+  const firebaseDB = firebase.database();
+
+  firebaseDB.ref('matches').once('value').then((snapshot)=>{
+      console.log(snapshot.val())
+  })
