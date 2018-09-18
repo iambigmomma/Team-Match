2ee39f8810a8a5e5c438dfd9190710d69c834280
iambigmomma@gmail.com <thebassist27>
2018-09-18 09:17:57 +0200

    Add private and public routes

---
+++ src/Components/admin/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AdminLayout from '../../Hoc/Admin_layout';
+
+const Dashboard = () => {
+    return (
+        <AdminLayout>
+            <div className="user_dashboard">
+                This is Dashboard
+            </div>
+        </AdminLayout>
+    );
+};
+
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 import SignIn from './Components/signin';
+import Dashboard from './Components/admin/Dashboard';
 
 import Home from './Components/home';
+import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
 
 const Routes = (props) => {
     return (
         <Layout>
             <Switch>
-                <Route exact component={SignIn} path="/sign_in"/>
-                <Route exact component={Home} path="/"/>
+                <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
+                <PublicRoute {...props} restricted={true} exact component={SignIn} path="/sign_in"/>
+                <PublicRoute {...props} restricted={false} exact component={Home} path="/"/>
             </Switch>       
         </Layout>
     );
